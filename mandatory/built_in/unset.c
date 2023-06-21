8d8bdd0657dae70933db80b4d0126092d3f96489
tnantaki <tinyhave@gmail.com>
2023-06-21 15:00:12 +0700

    Fix unset

--- mandatory/built_in/unset.c
+++ mandatory/built_in/unset.c
@@ -29,7 +29,7 @@ int ft_unset(char **arg)
 	{
 		if (!check_valid_var(arg[i], &status, "unset: `"))
 			continue;
-		env = search_env_var(arg[i], ft_var_len(arg[i]));
+		env = search_env_var(arg[i], ft_strlen(arg[i]));
 		if (env)
 			del_env(env, 0);
 	}
